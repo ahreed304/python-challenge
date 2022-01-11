e526e488f6ae211ec55b8c023772f82020d05b39
Austin Reed <austinhreed@protonmail.com>
2022-01-11 00:39:06 -0600

    added nested county results (temp)

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,16 +5,19 @@ project_root = os.path.dirname(__file__)
 csv_path = os.path.join(project_root, "Resources", "election_data.csv")
 
 results = {}
-bar = {}
-
 
 with open(csv_path,'r') as file:
     reader = DictReader(file)
     for row in reader:
         candidate = row['Candidate']
         if candidate not in results:
-            results[candidate] = {'votes': 0, 'percentage': 0.00}
+            results[candidate] = {'votes': 0, 'percentage': 0.00, 'by_county': {}}
         results[candidate]['votes'] += 1
+
+        county = row['County']
+        if county not in results[candidate]['by_county']:
+            results[candidate]['by_county'][county] = {'votes': 0, 'percentage': 0.00}
+        results[candidate]['by_county'][county]['votes'] += 1
 
 total_votes = sum([results[candidate]['votes'] for candidate in results])
 for candidate in results:
