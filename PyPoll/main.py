9efe0f5945875e6bde90cebaa1445bf4b0f53650
Austin Reed <austinhreed@protonmail.com>
2022-01-10 22:55:56 -0600

    added main vote tabulation logic

---
+++ PyPoll/main.py
@@ -0,0 +1,31 @@
+import os
+from csv import DictReader
+
+project_root = os.path.dirname(__file__)
+csv_path = os.path.join(project_root, "Resources", "election_data.csv")
+
+results = {}
+bar = {}
+
+
+with open(csv_path,'r') as file:
+    reader = DictReader(file)
+    for row in reader:
+        candidate = row['Candidate']
+        if candidate not in results:
+            results[candidate] = {'votes': 0, 'percentage': 0.00}
+        results[candidate]['votes'] += 1
+
+total_votes = sum([results[candidate]['votes'] for candidate in results])
+for candidate in results:
+    results[candidate]['percentage'] = results[candidate]['votes'] / total_votes * 100
+
+print(results)
+
+
+
+
+        
+    
+
+
